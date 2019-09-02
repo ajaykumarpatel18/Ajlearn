c13093beb7f26c171d6ffaeb9c46391f3ff8e9f7
Admin <admin@Aj.local>
2019-09-02 13:12:43 -0700

    Adding to repo

--- src/main/java/com/xyz/App.java
+++ src/main/java/com/xyz/App.java
@@ -3,7 +3,7 @@ package com.xyz;
 public class App {
 
         public static void main(String[] args) {
-                System.out.println("Hello World of Ajay !");
+                System.out.println("Hello World of Ajay 2!");
         }
 
 }
